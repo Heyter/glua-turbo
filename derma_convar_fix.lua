f901e6f6d50c15848501146724afb45e3b62726b
Sam Nyashev <12237569+Heyter@users.noreply.github.com>
2024-12-10 11:47:56 +0300

    Update derma_convar_fix.lua

--- derma_convar_fix.lua
+++ derma_convar_fix.lua
@@ -1,8 +1,7 @@
-
 local cacheKeys, cache, len = {}, {}, 0
-local name
 timer.Create("derma_convar_fix", 0.5, 0,  function()
 	if len == 0 then return end
+	local name
 	for i = 1, len do
 		name = cache[i]
 		RunConsoleCommand(name, cacheKeys[name])
@@ -18,43 +17,44 @@ function Derma_SetCvar_Safe(name, value)
 		len = len + 1
 		cache[len] = name
 	else
+		timer.Adjust("derma_convar_fix", 0.5)
 		cacheKeys[name] = tostring(value)
 	end
 end
 
-function Derma_Install_Convar_Functions( PANEL )
-	function PANEL:SetConVar( strConVar )
+function Derma_Install_Convar_Functions(PANEL)
+	function PANEL:SetConVar(strConVar)
 		self.m_strConVar = strConVar
 	end
 
-	function PANEL:ConVarChanged( strNewValue )
+	function PANEL:ConVarChanged(strNewValue)
 		local cvar = self.m_strConVar
-		if ( not cvar or string.len(cvar) < 2 ) then return end
+		if not cvar or string.len(cvar) < 2 then return end
 		Derma_SetCvar_Safe(cvar, strNewValue)
 	end
 
 	-- Todo: Think only every 0.1 seconds?
 	function PANEL:ConVarStringThink()
 		local cvar = self.m_strConVar
-		if ( not cvar or string.len(cvar) < 2 ) then return end
+		if not cvar or string.len(cvar) < 2 then return end
 
 		local strValue = GetConVarString(cvar)
-		if ( self.m_strConVarValue == strValue ) then return end
+		if self.m_strConVarValue == strValue then return end
 
 		self.m_strConVarValue = strValue
-		self:SetValue( strValue )
+		self:SetValue(strValue)
 	end
 
 	function PANEL:ConVarNumberThink()
 		local cvar = self.m_strConVar
-		if ( not cvar or string.len(cvar) < 2 ) then return end
-		local numValue = GetConVarNumber( cvar )
+		if not cvar or string.len(cvar) < 2 then return end
+		local numValue = GetConVarNumber(cvar)
 
 		-- In case the convar is a "nan"
-		if ( numValue != numValue ) then return end
-		if ( self.m_strConVarValue == numValue ) then return end
+		if numValue ~= numValue then return end
+		if self.m_strConVarValue == numValue then return end
 
 		self.m_strConVarValue = numValue
-		self:SetValue( numValue )
+		self:SetValue(numValue)
 	end
 end
