cc84d781404b6215956a1d18745e31d918e166fe
Sam Nyashev <12237569+Heyter@users.noreply.github.com>
2025-03-27 18:39:04 +0300

    Update hook.lua

--- hook.lua
+++ hook.lua
@@ -1,4 +1,4 @@
-local debug_info = debug.getinfo
+local debug_getinfo = debug.getinfo
 local isstring = isstring
 local isfunction = isfunction
 local IsValid = IsValid
@@ -9,45 +9,50 @@ local hook_index = {}
 local hook_id = {}
 
 local function GetTable() -- This function is now slow
-	local ret = {}
-	for name, callbacks in pairs(hook_callbacks) do
-		ret[name] = {}
-		for index, callback in pairs(callbacks) do
-			ret[name][hook_id[name][index]] = callback
-		end
-	end
-	return ret
+    local ret = {}
+    for name, callbacks in pairs(hook_callbacks) do
+        ret[name] = {}
+        local ids = hook_id[name]
+        for index = 1, #callbacks do
+            ret[name][ids[index]] = callbacks[index]
+        end
+    end
+    return ret
 end
 
+-- hook.Exists("HUDPaint", "test")
 local function Exists(name, id)
-	return (hook_index[name] ~= nil) and (hook_index[name][id] ~= nil)
+    local index = hook_index[name]
+    return index and index[id] ~= nil
 end
 
 -- hook.HasHook("HUDPaint")
 local function HasHook(event_name)
-	return hook_index[event_name] ~= nil
+    return hook_index[event_name] ~= nil
 end
 
 -- hook.GetHook("HUDPaint", "test")
-local function GetHook(name, uniq)
-	local id = (hook_index[name] ~= nil) and hook_index[name][uniq]
-
-	return id and hook_callbacks[name][id]
+local function GetHook(name, uniq) -- @return function
+    local index = hook_index[name]
+    if not index then return end
+    
+    local id = index[uniq]
+    return id and hook_callbacks[name][id]
 end
 
 local function Call(name, gm, ...)
 	local callbacks = hook_callbacks[name]
 
-	if (callbacks ~= nil) then
+	if callbacks ~= nil then
 		local i = #callbacks
 
 		if i > 0 then
 			::runhook::
 			local v = callbacks[i]
-			if (v ~= nil) then
+			if v ~= nil then
 				local a, b, c, d, e, f = v(...)
 
-				if (a ~= nil) then
+				if a ~= nil then
 					return a, b, c, d, e, f
 				end
 
@@ -57,14 +62,10 @@ local function Call(name, gm, ...)
 		end
 	end
 
-	if (not gm) then
-		return
-	end
+	if not gm then return end
 
 	local callback = gm[name]
-	if (not callback) then
-		return
-	end
+	if not callback then return end
 
 	return callback(gm, ...)
 end
@@ -75,32 +76,24 @@ end
 
 local function Remove(name, id)
 	local callbacks = hook_callbacks[name]
-
-	if (not callbacks) then
-		return
-	end
+	if not callbacks then return end
 
 	local indexes = hook_index[name]
 	local index = indexes[id]
-
-	if (not index) then
-		return
-	end
+	if not index then return end
 
 	local count = #callbacks
+	local ids = hook_id[name]
 
-	if (count == index) then
+	if count == index then
 		callbacks[index] = nil
 		indexes[id] = nil
-		hook_id[name][index] = nil
+		ids[index] = nil
 	else
-		local ids = hook_id[name]
-
 		callbacks[index] = callbacks[count]
 		callbacks[count] = nil
 
 		local lastid = ids[count]
-
 		indexes[id] = nil
 		indexes[lastid] = index
 
@@ -110,42 +103,31 @@ local function Remove(name, id)
 end
 
 local function Add(name, id, callback)
-	if isfunction(id) then
-		callback = id
-		id = debug_info(callback).short_src
-	end
+    if isfunction(id) then
+        callback = id
+        id = debug_getinfo(callback, "S").short_src
+    end
 
-	if (not callback) then
-		return
-	end
+	if not callback then return end
+    if hook_callbacks[name] == nil then
+        hook_callbacks[name] = {}
+        hook_index[name] = {}
+        hook_id[name] = {}
+    end
 
-	if (hook_callbacks[name] == nil) then
-		hook_callbacks[name] = {}
-		hook_index[name] 	 = {}
-		hook_id[name] 	 = {}
-	end
+    if Exists(name, id) then Remove(name, id) end
+    local callbacks = hook_callbacks[name]
+    local indexes = hook_index[name]
 
-	if Exists(name, id) then
-		Remove(name, id) -- properly simulate hook overwrite behavior
-	end
-
-	local callbacks = hook_callbacks[name]
-	local indexes = hook_index[name]
-
-	if (not isstring(id)) then
+	if not isstring(id) then
 		local orig = callback
 		callback = function(...)
-			if IsValid(id) then
-				return orig(id, ...)
-			end
-
+			if IsValid(id) then return orig(id, ...) end
 			local index = indexes[id]
 			Remove(name, id)
 
 			local nextcallback = callbacks[index]
-			if (nextcallback ~= nil) then
-				return nextcallback(...)
-			end
+			if nextcallback ~= nil then return nextcallback(...) end
 		end
 	end
 
@@ -155,18 +137,7 @@ local function Add(name, id, callback)
 	hook_id[name][index] = id
 end
 
-
 hook = setmetatable({
-	Remove = Remove,
-	GetTable = GetTable,
-	Exists = Exists,
-	HasHook = HasHook,
-	Add = Add,
-	Call = Call,
-	GetHook = GetHook,
-	Run = Run
-}, {
-	__call = function(self, ...)
-		return self.Add(...)
-	end
-})
+	Remove = Remove, GetTable = GetTable, Exists = Exists, HasHook = HasHook,
+	Add = Add, Call = Call, GetHook = GetHook, Run = Run
+}, { __call = function(self, ...) return self.Add(...) end })
